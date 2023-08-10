2b3b8724cca2a1805215575c00a827b92d52c93b
Hilmi Güner <hilmi.guner@hotmail.com>
2023-08-10 16:34:38 +0300

    Slider Üst Haber'lere tıklama işlevselliği eklendi

--- components/SliderNewsTopItem.js
+++ components/SliderNewsTopItem.js
@@ -1,16 +1,29 @@
 // React Native Temel Paketler
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 // Statik Değerler
 import imageMapping from "../constants/imageMapping";
 import AppColors from "../constants/colors";
 
+// React Navigation
+import { useNavigation } from "@react-navigation/native";
+
 function SliderNewsTopItem({ imageName, newsTitle }) {
     const imagePath = imageMapping[imageName];
+
+    const navigation = useNavigation();
+    function newsOnPressHandler() {
+        navigation.navigate("NewsDetailsScreen");
+    }
+
     return(
         <View style={styles.rootContainer}>
-            <Image style={styles.image} source={imagePath}/>
-            <Text style={styles.text}>{newsTitle}</Text>
+            <TouchableOpacity onPress={newsOnPressHandler}>
+                <Image style={styles.image} source={imagePath}/>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={newsOnPressHandler}>
+                <Text style={styles.text}>{newsTitle}</Text>
+            </TouchableOpacity>
         </View>
     );
 }
