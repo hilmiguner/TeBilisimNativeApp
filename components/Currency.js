0417bb761de72af0a176162816b60d58bd37b452
Hilmi Güner <hilmi.guner@hotmail.com>
2023-08-15 11:01:27 +0300

    Döviz öğesinin arkaplanına Linear Gradient eklendi

--- components/Currency.js
+++ components/Currency.js
@@ -7,13 +7,16 @@ import CurrencyItem from "./CurrencyItem";
 // Statik Değerler
 import AppColors from "../constants/colors";
 
+// Linear Gradient Paketi
+import LinearGradient from "react-native-linear-gradient";
+
 function Currency() {
     return(
-        <View style={styles.rootContainer}>
+        <LinearGradient colors={["#541B8B", "#14035C"]} style={styles.rootContainer}>
             <CurrencyItem icon="attach-money" currentValue="17,29" deltaValue="+0,34" size={24} color={AppColors.yellow} isIncreasing={true} currencyName="DOLAR"/>
             <CurrencyItem icon="euro" currentValue="18,64" deltaValue="-0,34" size={24} color={AppColors.yellow} isIncreasing={false} currencyName="EURO"/>
             <CurrencyItem icon="diamond" currentValue="970,64" deltaValue="-99,34" size={24} color={AppColors.yellow} isIncreasing={false} currencyName="ALTIN"/>
-        </View>
+        </LinearGradient>
     );
 }
 
@@ -21,7 +24,6 @@ export default Currency;
 
 const styles = StyleSheet.create({
     rootContainer: {
-        backgroundColor: "purple",
         flexDirection: "row",
         justifyContent: "space-between",
         padding: 16,
