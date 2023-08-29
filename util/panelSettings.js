66476e71c5a27d2eed2734d715c289765e9e25b3
Hilmi Güner <hilmi.guner@hotmail.com>
2023-08-29 11:46:59 +0300

    Panelden gelecek ayarlar verisi için şablon oluşturuldu

---
+++ util/panelSettings.js
@@ -0,0 +1,53 @@
+export default PanelSettings = {
+    oneSignalAppID: "",
+    oneSignalAPI_Key: "",
+    iosStatusBarContentColor: "", // "Beyaz" veya "Siyah"
+    headerAndStatusBarBG_Color: "", // Renk
+    menuTextColor: "", // Renk
+    headerTextColor: "", // Renk
+    menuBG_Color: "", // Renk
+    themePrimaryColor: "", // Renk
+    moduleTitleTextColor: "", // Renk
+    detailTextColor: "", // Renk
+    adMobIosBannerID: "",
+    adMobAndroidBannerID: "",
+    adMobIosInterstitialID: "",
+    adMobAndroidInterstitialID: "",
+    tvStreamLink: "",
+    radioStreamLink: "",
+    footerShortDescription: "",
+    breakingNewsModule: "", // "Açık" veya "Kapalı"
+    biographyModule: "", // "Açık" veya "Kapalı"
+    interviewModule: "", // "Açık" veya "Kapalı"
+    quintipleCuffsModule: "", // "Açık" veya "Kapalı"
+    dailyCuffsModule: "", // "Açık" veya "Kapalı"
+    highlightedNewsModule: "", // "Açık" veya "Kapalı"
+    localNewsModule: "", // "Açık" veya "Kapalı"
+    trendNewsModule: "", // "Açık" veya "Kapalı"
+    tabbedNewsModule: "", // "Açık" veya "Kapalı"
+    prayerTimesModule: "", // "Açık" veya "Kapalı"
+    topCuffsModule: "", // "Açık" veya "Kapalı"
+    mainCuffTitles: "", // "Açık" veya "Kapalı"
+    categoryCuffTitles: "", // "Açık" veya "Kapalı"
+    authorsModule: "", // "Açık" veya "Kapalı"
+    weatherModule: "", // "Açık" veya "Kapalı"
+    marketsModule: "", // "Açık" veya "Kapalı"
+    videosModule: "", // "Açık" veya "Kapalı"
+    galleriesModule: "", // "Açık" veya "Kapalı"
+    leagueScoreStatusModule: "", // "Açık" veya "Kapalı"
+    authorsDirectionType: "", // "Dikey" veya "Yatay"
+    videosDirectionType: "", // "Dikey" veya "Yatay"
+    galleriesDirectionType: "", // "Dikey" veya "Yatay"
+    highlightedNewsDirectionType: "", // "Dikey" veya "Yatay"
+    weatherAndMarketsDirectionType: "", // "Dikey" veya "Yatay"
+    otherNewsViewType: "", // "Özel" veya "Düz"
+    detailFontSize: "", // Sayı
+    primaryFontFamily: "", // Font Family (sadece detaylar ve spot)
+    secondaryFontFamily: "", // Font Family (modül ve detay başlıkları)
+    cuffImageHeight: "", // Sayı
+    cuffImageRatio: "", // Sayı
+    detailTitleFontSize: "", // Sayı
+    mainPageCategoryIDs: "", // Dizi (Örneğin '1,4,5,123,5')
+    app_adsContentCode: "",
+    mainCuffLimit: "", // Sayı
+};
