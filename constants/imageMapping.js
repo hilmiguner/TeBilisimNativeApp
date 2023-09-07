958631c16c6212de8e156dd7248979a35906faf9
Hilmi Güner <hilmi.guner@hotmail.com>
2023-09-07 11:08:05 +0300

    imageMapping verilerinden daha önceden hazırlanmış statik resim dosyalarının yolunu tutan veriler temizlendi

--- constants/imageMapping.js
+++ constants/imageMapping.js
@@ -1,8 +1,6 @@
 const imageMapping = {
     SliderNewsTop1: require("../assets/images/snake.jpeg"),
     SliderNewsTop2: require("../assets/images/baby.jpeg"),
-    SliderDots1: require("../assets/images/sliderdots1.jpeg"),
-    SliderDots2: require("../assets/images/sliderdots2.jpeg"),
     Author1: require("../assets/images/author1.jpeg"),
     Author2: require("../assets/images/author2.jpeg"),
     VideoGallery1: require("../assets/images/videoGallery1.jpeg"),
