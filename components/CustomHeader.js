09cec8e0aabd00ed0c164f4da801725589ad7ae0
Hilmi Güner <hilmi.guner@hotmail.com>
2023-12-05 22:55:33 +0300

    CustomHeader bileşeninin useSafeArea özelliği, modal pencereler için geliştirildi

--- components/CustomHeader.js
+++ components/CustomHeader.js
@@ -7,13 +7,14 @@ function CustomHeader({ left, center, right, backgroundColor, useSafeArea }) {
     const safeAreaInsets = useSafeAreaInsets();
 
     const paddingTop = useSafeArea ? safeAreaInsets.top : 0;
+    const height = useSafeArea ? SCREEN_HEIGHT*0.13 : (SCREEN_HEIGHT*0.13) - safeAreaInsets.top;
 
     const leftComponent = <View style={{ flex: 1, flexDirection: "row", justifyContent: "flex-start" }}>{left}</View>
     const centerComponent = <View style={{ flex: 1, flexDirection: "row", justifyContent: "center" }}>{center}</View>
     const rightComponent = <View style={{ flex: 1, flexDirection: "row", justifyContent: "flex-end" }}>{right}</View>
 
     return(
-        <View style={[styles.rootContainer, { backgroundColor: backgroundColor }]}>
+        <View style={[styles.rootContainer, { backgroundColor: backgroundColor, height: height }]}>
             <View style={[styles.innerContainer,  { paddingTop: paddingTop, justifyContent: "space-between" }]}>
                 {leftComponent}
                 {centerComponent}
@@ -28,7 +29,6 @@ export default CustomHeader;
 const styles = StyleSheet.create({
     rootContainer: {
         width: "100%",
-        height: SCREEN_HEIGHT*0.13,
         justifyContent: "center"
     },
     innerContainer: {
