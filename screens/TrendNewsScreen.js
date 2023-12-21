a34ac6cee2d499a66d9b26c20041c90e3b8a6e5c
Hilmi Güner <hilmi.guner@hotmail.com>
2023-12-21 22:14:38 +0300

    Trend haberler sayfası düzenlendi

--- screens/TrendNewsScreen.js
+++ screens/TrendNewsScreen.js
@@ -22,12 +22,12 @@ function TrendNewsScreen({ navigation }) {
     let content = <ActivityIndicator/>;
 
     if(news) {
-        const carouselData = news.length > 10 ? news.slice(0, 10) : news;
-        const otherData = news.length > 10 ? news.slice(10, news.length) : null;
+        const carouselData = news.length > 10 ? news.slice(0, 10) : null;
+        const otherData = news.length > 10 ? news.slice(10, news.length) : news;
         content = (
             <ScrollView>
-                <CarouselTrendNews data={carouselData}/>
-                { otherData && <OtherTrendNews newsData={otherData}/> }
+                { carouselData && <CarouselTrendNews data={carouselData}/> }
+                <OtherTrendNews newsData={otherData}/>
             </ScrollView>
         );   
     }
